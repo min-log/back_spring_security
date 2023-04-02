0900b43bec58a543690881da9a076bcb52a6a68d
jimin <minlog.0430@gmail.com>
2023-04-03 02:35:20 +0900

    login

--- server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,21 +1,85 @@
 package com.sp.fc.web.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final CustomAuthDetails customAuthDetails;
+
+    public SecurityConfig(CustomAuthDetails customAuthDetails) {
+        this.customAuthDetails = customAuthDetails;
+    }
+
+    //회원권한
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser(
+                User.withDefaultPasswordEncoder()
+                        .username("user1")
+                        .password("1111")
+                        .roles("USER")
+        ).withUser(
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("2222")
+                        .roles("ADMIN")
+        );
+    }
+
+    //관리자는 user페이지에 접근 가능하도록 설정
+    @Bean
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(request->{
                     request
-                            .antMatchers("/**").permitAll()
+                            .antMatchers("/").permitAll()
+                            .anyRequest().authenticated()
                             ;
                 })
+
+                .formLogin(
+                        login -> login.loginPage("/login") //로그인 페이지 경로로 이동
+                                .permitAll() //권한 없이도 접근가능하게 설정
+                                .defaultSuccessUrl("/",false) //로그인 성공시 페이지 설정
+                                .failureUrl("/login-error") //로그인 실패 페이지 설정
+                                .authenticationDetailsSource(customAuthDetails)
+
+                ) //user name password
+                .logout(
+                        logout-> logout.logoutSuccessUrl("/") //로그아웃 성공시 페이지
+                )
+                .exceptionHandling( //관리자 페이지
+                        exception-> exception.accessDeniedPage("/access-denied")
+                )
                 ;
+    }
+
+
+    //리소스 시큐리티 제거
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
     }
 }
--- server/login-basic/src/main/java/com/sp/fc/web/controller/HomeController.java
+++ server/login-basic/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -2,9 +2,12 @@ package com.sp.fc.web.controller;
 
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -24,6 +27,14 @@ public class HomeController {
         model.addAttribute("loginError", true);
         return "loginForm";
     }
+
+
+    @ResponseBody
+    @GetMapping("/auth")
+    public Authentication auth(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
 
     @GetMapping("/access-denied")
     public String accessDenied(){
