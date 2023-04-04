9a05fd3893573e38e1df0a0c81e28175568f3d3b
jimin <minlog.0430@gmail.com>
2023-04-04 17:26:54 +0900

    권한별  토큰설정

--- server/login-custom-filter/src/main/java/com/sp/fc/web/controller/HomeController.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.sp.fc.web.controller;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -23,6 +26,13 @@ public class HomeController {
         model.addAttribute("loginError", true);
         return "loginForm";
     }
+
+    @ResponseBody
+    @GetMapping("/auth")
+    public Authentication auth(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
 
     @GetMapping("/access-denied")
     public String accessDenied(){
